825b872e745757bc829a2b33be0f02669dfc02a2
musyoku <musyoku@users.noreply.github.com>
2016-09-16 13:47:48 +0900

    j

--- test/model.py
+++ test/model.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
+import json, os
 from args import args
-from wavenet import WaveNet, Conf
+from wavenet import WaveNet, Params
 
-conf = Conf()
-conf.gpu_enabled = True if args.gpu_enabled == 1 else False
+# load params.json
+try:
+	os.mkdir(args.params_dir)
+except:
+	pass
+filename = args.params_dir + "/{}".format(args.params_filename)
+if os.path.isfile(filename):
+	f = open(filename)
+	dict = json.load(f)
+	params = Params(dict)
+else:
+	params = Params()
+	params.gpu_enabled = True if args.gpu_enabled == 1 else False
+	params.audio_channels = 7
+	params.residual_conv_kernel_size = 2
+	params.residual_conv_channels = [3]
 
-conf.audio_channels = 7
+	f = open(filename, "w")
+	json.dump(params.to_dict(), f, indent=4)
 
-conf.residual_conv_kernel_size= 2
-conf.residual_conv_channels = [3]
-
-wavenet = WaveNet(conf)
+params.dump()
+wavenet = WaveNet(params)
 wavenet.load(args.model_dir)
--- wavenet.py
+++ wavenet.py
@@ -16,33 +16,73 @@ activations = {
 	"elu": F.elu
 }
 
-class Conf():
-	def __init__(self):
+class Params():
+	def __init__(self, dict=None):
 		self.audio_channels = 256
 
-		self.residual_conv_kernel_size = 2
+		# entire architecture
+		# causal conv stack -> residual dilated conv stack -> skip-connections conv -> softmax
 
+		self.causal_conv_no_bias = True
+		self.causal_conv_kernel_size = 2
+		self.causal_conv_apply_batchnorm = True
+		self.causal_conv_batchnorm_before_conv = True
 		# [<- input   output ->]
-		# audio input -> conv -> (256,) -> conv -> (256,) -> conv -> (256,) 
-		# to stack more layer, do [256, 256, 256, 256, 256, 256, ...]
-		self.residual_conv_channels= [256, 256, 256]
+		# audio input -> conv -> (128,) -> conv -> (128,) -> conv -> (128,) -> residual dilated conv stack
+		self.causal_conv_channels = [128, 128, 128]
+
 		self.residual_conv_no_bias = True
-
+		self.residual_conv_kernel_size = 2
+		self.residual_conv_apply_batchnorm = True
+		self.residual_conv_batchnorm_before_conv = True
 		# [<- input   output ->]
-		# skip-connections -> ReLu -> conv -> (256,) -> ReLU -> conv -> (256,) -> softmax -> prediction
-		self.skip_connections_conv_channels = [256, 256]
+		# causal conv output (128,) -> conv -> (32,) -> 1x1 conv -> (128,) -> conv -> (32,) -> 1x1 conv -> (128,) -> ...
+		self.residual_conv_channels = [32, 32, 32]
+		# [<- input   output ->]
+		self.residual_conv_dilations = [1, 2, 4, 8, 2, 4, 8, 4, 8]
 
-		self.apply_batchnorm = False
+		self.skip_connections_conv_no_bias = False
+		self.skip_connections_conv_kernel_size = 2
+		self.skip_connections_conv_apply_batchnorm = True
+		self.skip_connections_conv_batchnorm_before_conv = True
+		# [<- input   output ->]
+		# skip-connections -> ReLU -> conv -> (128,) -> ReLU -> conv -> (256,) -> softmax -> prediction
+		self.skip_connections_conv_channels = [128, 256]
+
 		self.gpu_enabled = True
-		self.learning_rate = 0.0003
+		self.learning_rate = 0.001
 		self.gradient_momentum = 0.9
 		self.gradient_clipping = 10.0
 
+		if dict:
+			self.from_dict(dict)
+
+	def from_dict(self, dict):
+		for attr, value in dict.iteritems():
+			if not hasattr(self, attr):
+				raise Exception("invalid parameter '{}'".format(attr))
+			setattr(self, attr, value)
+
+	def to_dict(self):
+		dict = {}
+		for attr, value in self.__dict__.iteritems():
+			dict[attr] = value
+		return dict
+
+	def dump(self):
+		print "params:"
+		for attr, value in self.__dict__.iteritems():
+			print "	{}: {}".format(attr, value)
+
 	def check(self):
-		base = Conf()
+		base = Params()
 		for attr, value in self.__dict__.iteritems():
 			if not hasattr(base, attr):
 				raise Exception("invalid parameter '{}'".format(attr))
+		if self.causal_conv_channels[-1] != self.skip_connections_conv_channels[0]:
+			raise Exception("causal_conv_channels[-1] != skip_connections_conv_channels[0]")
+		if self.audio_channels != self.skip_connections_conv_channels[-1]:
+			raise Exception("audio_channels != skip_connections_conv_channels[-1]")
 
 def sum_sqnorm(arr):
 	sq_sum = collections.defaultdict(float)
@@ -96,16 +136,16 @@ class ResidualConvLayer(chainer.Chain):
 
 class WaveNet():
 
-	def __init__(self, conf):
-		conf.check()
-		self.conf = conf
+	def __init__(self, params):
+		params.check()
+		self.params = params
 
 		# stack residual blocks
-		ksize = (conf.residual_conv_kernel_size, 1)
-		nobias = conf.residual_conv_no_bias
+		ksize = (params.residual_conv_kernel_size, 1)
+		nobias = params.residual_conv_no_bias
 		self.residual_conv_layers = []
-		channels = [(conf.audio_channels, conf.residual_conv_channels[0])]
-		channels += zip(conf.residual_conv_channels[:-1], conf.residual_conv_channels[1:])
+		channels = [(params.audio_channels, params.residual_conv_channels[0])]
+		channels += zip(params.residual_conv_channels[:-1], params.residual_conv_channels[1:])
 		for i, (n_in, n_out) in enumerate(channels):
 			w_shape = (n_out, n_in, ksize[0], ksize[1])
 			initial_w = np.ones(w_shape).astype(np.float32)
@@ -115,17 +155,17 @@ class WaveNet():
 			attributes["projection"] = L.Convolution2D(n_out, n_in, 1, stride=1)
 			attributes["batchnorm"] = L.BatchNormalization(n_in)
 			conv_layer = ResidualConvLayer(**attributes)
-			conv_layer.apply_batchnorm = conf.apply_batchnorm
+			conv_layer.apply_batchnorm = params.apply_batchnorm
 			self.residual_conv_layers.append(conv_layer)
 
 	@property
 	def input_receptive_size(self):
-		conf = self.conf
-		return conf.residual_conv_kernel_size ** (len(conf.residual_conv_channels) + 1)
+		params = self.params
+		return params.residual_conv_kernel_size ** (len(params.residual_conv_channels) + 1)
 
 	@property
 	def gpu_enabled(self):
-		return self.conf.gpu_enabled
+		return self.params.gpu_enabled
 	
 
 	def forward_residual(self, x_batch):
